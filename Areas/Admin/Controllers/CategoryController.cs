9447bc4e5def3e7b1719a71761a14287855e8814
Mousumi <dutta.mousumi136@gmail.com>
2022-07-21 10:41:53 -0400

    Create Book Store Project

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BookStore.Data;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //GET
+                
+        public async Task<IActionResult> Index()
+        {
+            
+            return View(await _context.Category.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Method for CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category) 
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Category.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Text;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
